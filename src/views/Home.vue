301e49a63b5855f2b152a3dae9501fa35e8f72af
unknown <rizal.bahriawan@pusilkom.com>
2026-04-07 15:26:03 +0700

    add home

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home d-flex justify-center mt-12">
+    <div class="pt-12">
+      <h1 class="text-center">Dashboard Mosq v 0.1</h1>
+      <h2 class="text-center mt-8">Assalamualaikum Warahmatullahi Wabarakatuh</h2>
+    </div>
+  </div>
+</template>
